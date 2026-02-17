aed1b0fdf651483da09ec3ebf060a180ed3343f2
Popoo2020 <arimon.fi@gmail.com>
2026-02-16 17:13:44 -0800

    Add package init file

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""Package initialiser for Sentinel‑AI‑AutoTriage."""
+
+__all__ = [
+    "SentinelConfig",
+    "get_sentinel_client",
+    "list_active_incidents",
+    "update_incident_status",
+    "LLMClient",
+    "IncidentSummary",
+]
